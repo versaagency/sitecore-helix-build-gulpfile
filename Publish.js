2ca7632316685b1b8e2e059ed163668ba84d1821
Brendan McKenzie <brendan.mckenzie@deepend.melbourne>
2018-08-03 10:47:34 +1000

    Fix issue with Publish task

--- Publish.js
+++ Publish.js
@@ -1,17 +1,10 @@
 const gulp = require('gulp')
-const runSequence = require('run-sequence')
-const { config } = require('./config')
 
-gulp.task('Publish', callback => runSequence(
-  [
-    'Run-Webpack',
-    'Publish-Configs',
-    'Publish-Views',
-    'Publish-Serialisation',
-    'Publish-Sitecore-Modules',
-    'Publish-Binaries'
-  ]
-    .concat(config.hasXConnectModels ? ['Publish-XConnect-Models'] : []),
-
-  callback
+gulp.task('Publish', gulp.series(
+  'Run-Webpack',
+  'Publish-Configs',
+  'Publish-Views',
+  'Publish-Serialisation',
+  'Publish-Sitecore-Modules',
+  'Publish-Binaries'
 ))
