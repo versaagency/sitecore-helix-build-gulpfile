078b97c9884897eb352d9333d4a38e76f5580dd2
Brendan McKenzie <brendan.mckenzie@deepend.melbourne>
2018-08-23 13:20:52 +1000

    Updated Auto Publish to use src/dest

--- Auto-Publish.js
+++ Auto-Publish.js
@@ -1,8 +1,7 @@
 const gulp = require('gulp')
 const watch = require('gulp-watch')
 const rimraf = require('rimraf')
-const fs = require('fs')
-const _path = require('path')
+const path = require('path')
 const {
   config
 } = require('./config')
@@ -11,24 +10,23 @@ const sitecoreBinaries = require('./data/Sitecore-Binaries')
 gulp.task('Auto-Publish', () => {
   const paths = './src/{Project,Feature,Foundation}/*/code/{App_Config,Views,bin,sitecore}/**/*'
 
-  return watch(paths, ({
-    path,
-    event
-  }) => {
-    const parts = path.split(_path.sep)
-    const fileName = parts[parts.length - 1]
-    const base = parts.filter((_, index) => index <= parts.indexOf('code')).join(_path.sep)
-    const destPath = `${config.websiteRoot}${path.substr(base.length)}`
+  return watch(paths, (ev) => {
+    // TODO: this is gross
+    const fileName = path.basename(ev.path)
+    const parts = ev.path.split(path.sep)
+    const base = parts.filter((_, index) => index <= parts.indexOf('code')).join(path.sep)
+    const destPath = `${config.websiteRoot}${ev.path.substr(base.length)}`
 
     if (sitecoreBinaries.indexOf(fileName) !== -1) {
       console.log(`Ignoring Sitecore file: ${fileName}`)
     } else {
-      switch (event) {
+      switch (ev.event) {
         case 'change':
         case 'add':
-          console.log(`Publishing ${fileName}`)
-          // TODO: update this to use gulp.src(...).dest(...)
-          fs.copyFileSync(path, destPath)
+          console.log(`Publishing: ${fileName}`)
+
+          gulp.src(ev.path)
+            .pipe(gulp.dest(path.dirname(destPath)))
           break
         case 'unlink':
           console.log(`Removing: ${fileName}`)
