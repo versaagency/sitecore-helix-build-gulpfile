a3649cd6371f2b0a3e5dc0f656eebcd66cccb726
Brendan McKenzie <brendan.mckenzie@deepend.melbourne>
2018-09-12 08:48:18 +1000

    Optimised Publish task

--- Publish.js
+++ Publish.js
@@ -1,10 +1,26 @@
 const gulp = require('gulp')
+const foreach = require('gulp-foreach')
+const newer = require('gulp-newer')
+const debug = require('gulp-debug')
+const { config } = require('./config')
+const sitecoreBinaries = require('./data/Sitecore-Binaries')
 
-gulp.task('Publish', gulp.series(
-  'Run-Webpack',
-  'Publish-Configs',
-  'Publish-Views',
-  'Publish-Serialisation',
-  'Publish-Sitecore-Modules',
-  'Publish-Binaries'
-))
+gulp.task('Publish-Helix', () => {
+  const roots = './src/{Project,Feature,Foundation}/*/code/'
+  const files = '/{App_Config,Views,bin,sitecore}/**/*'
+  const destination = config.websiteRoot
+
+  return gulp.src(roots).pipe(
+    foreach((stream, file) => {
+      const glob = [file.path + files].concat(sitecoreBinaries.map(ent => `!./**/*/${ent}`))
+      gulp
+        .src(glob, { base: file.path })
+        .pipe(newer(destination))
+        .pipe(debug({ title: 'Copying' }))
+        .pipe(gulp.dest(destination))
+      return stream
+    })
+  )
+})
+
+gulp.task('Publish', gulp.parallel(['Run-Webpack', 'Publish-Helix']))
