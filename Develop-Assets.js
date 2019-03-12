844113045a93e13e871ae7a28c12347ec53d0275
Brendan McKenzie <brendan.mckenzie@deepend.melbourne>
2019-03-12 13:43:07 +1100

    Updated WDS to listen on all IPs

--- Develop-Assets.js
+++ Develop-Assets.js
@@ -7,7 +7,7 @@ gulp.task('Develop-Assets', () => {
   // Start a webpack-dev-server
   const compiler = webpack(config)
 
-  new WebpackDevServer(compiler, config.devServer).listen(8080, 'localhost', () => {
-    console.log('webpack-dev-server: http://localhost:8080/')
+  new WebpackDevServer(compiler, config.devServer).listen(8080, '0.0.0.0', () => {
+    console.log('webpack-dev-server: http://0.0.0.0:8080/')
   })
 })
