5559e39ab4e223dd26dbd368183626dc1ea2a2ad
Brendan McKenzie <brendan.mckenzie@deepend.melbourne>
2019-07-19 16:36:10 +1000

    Removed sln build scripts

--- Build.js
+++
@@ -1,3 +0,0 @@
-const gulp = require('gulp')
-
-gulp.task('Build', gulp.series('NuGet-Restore', 'Build-Solution', 'Publish'))
--- NuGet-Restore.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp')
-const nugetRestore = require('gulp-nuget-restore')
-const { config } = require('./config')
-
-gulp.task('NuGet-Restore', () => {
-  const solution = `./${config.solutionFile}`
-
-  return gulp.src(solution).pipe(nugetRestore())
-})
